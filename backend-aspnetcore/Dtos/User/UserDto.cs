13ae4f100881c24a5249c12c24305bfb2a9993d6
Nay Macbook <nayookyaw.nok@gmail.com>
2025-10-23 15:59:15 +1300

    add user model and user dto

---
+++ backend-aspnetcore/Dtos/User/UserDto.cs
@@ -0,0 +1,5 @@
+public class UserDto
+{
+    public string Username { get; set; }
+    public string Email { get; set; }
+}
---
+++ backend-aspnetcore/Models/User.cs
@@ -0,0 +1,6 @@
+public class User
+{
+    public Guid Id { get; set; }
+    public string Username { get; set; }
+    public string Email { get; set; }
+}
