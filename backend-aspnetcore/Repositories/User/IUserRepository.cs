2ea97dce188183ad0f4c6d9115c4dff014980697
Nay Macbook <nayookyaw.nok@gmail.com>
2025-10-23 19:52:10 +1300

    add user repo and interface

---
+++ backend-aspnetcore/Repositories/User/IUserRepository.cs
@@ -0,0 +1,8 @@
+public interface IUserRepository
+{
+    Task<User> GetUserByIdAsync(Guid id);
+    Task<IEnumerable<User>> GetAllUsersAsync();
+    // Task AddUserAsync(User user);
+    // Task UpdateUserAsync(User user);
+    // Task DeleteUserAsync(Guid id);
+}
