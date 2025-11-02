4620fc8837d91cde8ab3db0d4c5ba125d9116559
Nay Macbook <nayookyaw.nok@gmail.com>
2025-11-02 23:09:58 +1300

    add generate secure random stirng

--- backend-aspnetcore/Utils/TextUtil.cs
+++ backend-aspnetcore/Utils/TextUtil.cs
@@ -1,9 +1,22 @@
+using System.Security.Cryptography;
 
 namespace BackendAspNetCore.Utils;
+
 public class TextUtil
 {
-    public static string GenerateSecureRandomString (int lenght)
+    public static string GenerateSecureRandomString(int length)
     {
-        return "dd";
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        var byteArray = new byte[length];
+        var rng = RandomNumberGenerator.Create();
+        rng.GetBytes(byteArray);
+
+        char[] result = new char[length];
+        for (int i = 0; i < length; i++)
+        {
+            result[i] = chars[byteArray[i] % chars.Length];
+        }
+
+        return new string(result);
     }
 }
